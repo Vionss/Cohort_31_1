f810a5149d58a16838091268fb5341ab8632159b
vions <vions323@gmail.com>
2023-08-09 19:11:34 +0200

    lesson 17

--- lesson_16/homework_16/code/StudentInfo.java
+++ lesson_16/homework_16/code/StudentInfo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class StudentInfo {
     public static void main(String[] args) {
 
@@ -5,36 +7,47 @@ public class StudentInfo {
         // (String firstName, String lastName, MyDate birthday, String group, MyDate startCourse}
         //Создайте пару объектов этого класса. Напишите метод studentToString(Student student)
         // который возвращает строку с данными студента.
+        Scanner scanner = new Scanner(System.in);
+    }
+}
 
 
+       /* Student student = new Student();
+        MyDate startCourse = new MyDate();
 
-        MyDate myDate = new MyDate();
-        myDate.day = 3;
-        myDate.month = 10;
-        myDate.year = 1999;
+        System.out.println("Input first name: ");
+        student.firstName = scanner.nextLine();
+        System.out.println("Input last name: ");
+        student. lastName = scanner.nextLine();
 
-        MyDate myDate2 = new MyDate();
-        myDate2.day = 18;
-        myDate2.month = 7;
-        myDate2.year = 2023;
-
-        Student student = new Student();
-
-        student.firstName = "Rob";
-        student.lastName = "Kublar";
-        student.birthday = myDate;
-        student.group = "31";
-        student.startCourse = myDate2;
 
         System.out.println(studentToString(student));
 
     }
     //===Methods===
 
-    public static String studentToString(Student student){
-        return student.firstName + " "+ student.lastName+ " "+ student.birthday+ " "+ student.group+" "+ student.startCourse;
+    public static MyDate getMyDateFromScanner(Scanner scanner, String msg){
+        System.out.println(msg);
+        MyDate date = new MyDate();
+        System.out.println("Input day:");
+        date.day = scanner.nextInt();
+        System.out.println("Input month:");
+        date.month = scanner.nextInt();
+        System.out.println("Input year:");
+        date.year = scanner.nextInt();
+        scanner.nextLine();
+        return date;
 
+
+    }
+
+    public static String studentToString(Student student){
+        String firstName = student.toString();
+        String lastName = student.toString();
+        return student.firstName + " "+ student.lastName + "(" + (student.birthday)+ ")" + "group: ";
     }
 
     //=============
 }
+/*
+        */
---
+++ lesson_17/code/BankAccount/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Jack", "Johnson", "123-123-312312", new MyDate(1,2,2000));
+        Account account1 = new Account("DE1234567",123123,person1);
+        System.out.println(account1);
+
+
+
+
+
+
+    }
+    //===Methods===
+
+    //=============
+}
